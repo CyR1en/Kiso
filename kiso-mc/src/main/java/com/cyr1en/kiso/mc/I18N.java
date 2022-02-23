4cedea7a93c4db40386359509307e8730599bf70
CyR1en <cyrien3519@gmail.com>
2022-02-23 14:01:17 -0700

    Full support for UTF-8 Charset

--- kiso-mc/src/main/java/com/cyr1en/kiso/mc/I18N.java
+++ kiso-mc/src/main/java/com/cyr1en/kiso/mc/I18N.java
@@ -40,116 +40,123 @@ import java.util.ResourceBundle;
 @SuppressWarnings("UnstableApiUsage")
 public class I18N {
 
-  private final File propertyFile;
-  private final File propertiesDir;
-  private ResourceBundle bundle;
+    private final File propertyFile;
+    private final File propertiesDir;
+    private ResourceBundle bundle;
 
-  private String baseName;
-  private String fileName;
+    private String baseName;
+    private String fileName;
 
-  private JavaPlugin plugin;
+    private JavaPlugin plugin;
 
-  public I18N(JavaPlugin plugin, String baseName) {
-    this.plugin = plugin;
-    this.baseName = baseName;
-    fileName = baseName + ".properties";
-    propertyFile = new File(plugin.getDataFolder() + "/Properties", fileName);
-    propertiesDir = new File(plugin.getDataFolder() + "/Properties");
+    public I18N(JavaPlugin plugin, String baseName) {
+        this.plugin = plugin;
+        this.baseName = baseName;
+        fileName = baseName + ".properties";
+        propertyFile = new File(plugin.getDataFolder() + "/Properties", fileName);
+        propertiesDir = new File(plugin.getDataFolder() + "/Properties");
 
-    prepareFiles();
+        prepareFiles();
 
-    try {
-      InputStreamReader reader = new InputStreamReader(Files.newInputStream(propertyFile.toPath()), StandardCharsets.UTF_8);
-      bundle = new PropertyResourceBundle(reader);
-      if (needsUpdate())
-        update();
-    } catch (IOException e) {
-      e.printStackTrace();
+        try {
+            InputStreamReader reader = new InputStreamReader(
+                    Files.newInputStream(propertyFile.toPath()), StandardCharsets.UTF_8);
+            bundle = new PropertyResourceBundle(reader);
+            if (needsUpdate())
+                update();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private void prepareFiles() {
-    try {
-      Path dirPath = propertiesDir.toPath();
-      if (!Files.exists(dirPath)) {
-        Files.createDirectories(dirPath);
-      }
-      if (!Files.exists(propertyFile.toPath())) {
-        Files.createFile(propertyFile.toPath());
-        copyFiles(propertyFile);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+    private void prepareFiles() {
+        try {
+            Path dirPath = propertiesDir.toPath();
+            if (!Files.exists(dirPath)) {
+                Files.createDirectories(dirPath);
+            }
+            if (!Files.exists(propertyFile.toPath())) {
+                Files.createFile(propertyFile.toPath());
+                copyFiles(propertyFile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private void copyFiles(File file) {
-    try {
-      InputStream in = plugin.getResource(fileName);
-      OutputStream out = new FileOutputStream(file);
-      ByteStreams.copy(Objects.requireNonNull(in), out);
-    } catch (IOException e) {
-      e.printStackTrace();
+    private void copyFiles(File file) {
+        try {
+            InputStream in = plugin.getResource(fileName);
+            OutputStream out = new FileOutputStream(file);
+            ByteStreams.copy(Objects.requireNonNull(in), out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private void update() {
-    try {
-      ResourceBundle classPathBundle = new PropertyResourceBundle(plugin.getResource(fileName));
-      Enumeration<String> enumeration = classPathBundle.getKeys();
-      StringBuilder sb = new StringBuilder();
+    private void update() {
+        try {
+            InputStreamReader ir = new InputStreamReader(
+                    Objects.requireNonNull(plugin.getResource(fileName)), StandardCharsets.UTF_8);
+            ResourceBundle classPathBundle = new PropertyResourceBundle(ir);
+            Enumeration<String> enumeration = classPathBundle.getKeys();
+            StringBuilder sb = new StringBuilder();
 
-      while (enumeration.hasMoreElements()) {
-        String key = enumeration.nextElement();
-        sb.append("\n");
-        if (!bundle.containsKey(key))
-          sb.append(key).append(" = ").append(classPathBundle.getString(key));
-        else
-          sb.append(key).append(" = ").append(bundle.getString(key));
-      }
+            while (enumeration.hasMoreElements()) {
+                String key = enumeration.nextElement();
+                sb.append("\n");
+                if (!bundle.containsKey(key))
+                    sb.append(key).append(" = ").append(classPathBundle.getString(key));
+                else
+                    sb.append(key).append(" = ").append(bundle.getString(key));
+            }
 
-      BufferedWriter writer = new BufferedWriter(new FileWriter(propertyFile));
-      writer.write(sb.toString());
-      writer.flush();
-      writer.close();
+            BufferedWriter writer = Files.newBufferedWriter(propertyFile.toPath(), StandardCharsets.UTF_8);
+            writer.write(sb.toString());
+            writer.flush();
+            writer.close();
 
-      bundle = new PropertyResourceBundle(Files.newInputStream(propertyFile.toPath()));
-    } catch (IOException e) {
-      e.printStackTrace();
+            InputStreamReader ir2 = new InputStreamReader(
+                    Files.newInputStream(propertyFile.toPath()), StandardCharsets.UTF_8);
+            bundle = new PropertyResourceBundle(ir2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private boolean needsUpdate() {
-    boolean need = false;
-    try {
-      ResourceBundle classPathBundle = new PropertyResourceBundle(plugin.getResource(fileName));
-      Enumeration<String> enumeration = classPathBundle.getKeys();
-      while (!need && enumeration.hasMoreElements()) {
-        String key = enumeration.nextElement();
-        if (!bundle.containsKey(key))
-          need = true;
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+    private boolean needsUpdate() {
+        boolean need = false;
+        try {
+            InputStreamReader ir = new InputStreamReader(
+                    Objects.requireNonNull(plugin.getResource(fileName)), StandardCharsets.UTF_8);
+            ResourceBundle classPathBundle = new PropertyResourceBundle(ir);
+            Enumeration<String> enumeration = classPathBundle.getKeys();
+            while (!need && enumeration.hasMoreElements()) {
+                String key = enumeration.nextElement();
+                if (!bundle.containsKey(key))
+                    need = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return need;
     }
-    return need;
-  }
 
-  public String getFormattedProperty(String key, String... arguments) {
-    int placeHolderCount = StringUtils.countMatches(getProperty(key), "%s");
-    if (placeHolderCount < arguments.length || placeHolderCount == 0)
-      return getProperty(key);
-    return String.format(getProperty(key), (Object[]) arguments);
-  }
+    public String getFormattedProperty(String key, String... arguments) {
+        int placeHolderCount = StringUtils.countMatches(getProperty(key), "%s");
+        if (placeHolderCount < arguments.length || placeHolderCount == 0)
+            return getProperty(key);
+        return String.format(getProperty(key), (Object[]) arguments);
+    }
 
-  public String getProperty(String key) {
-    return getSafeProperty(key);
-  }
+    public String getProperty(String key) {
+        return getSafeProperty(key);
+    }
 
-  private String getSafeProperty(String key) {
-    if (bundle.containsKey(key))
-      return bundle.getString(key);
-    return String.format("MissingProperty[key = %s]", key);
-  }
+    private String getSafeProperty(String key) {
+        if (bundle.containsKey(key))
+            return bundle.getString(key);
+        return String.format("MissingProperty[key = %s]", key);
+    }
 
 }
